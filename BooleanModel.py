9886a4ce10bd247cb07311a3ecee726bd24ef640
Eduardo <eduardo00100@hotmail.com>
2023-03-11 23:12:51 -0300

    Indexacao do modelo booleano e arquivos do modelo
    Correcao do limite de 1M de caracteres, aumentando para 2M como split do documento. Criacao de arquivos para carregamento do modelo sem processar novamente.

--- BooleanModel.py
+++ BooleanModel.py
@@ -4,6 +4,8 @@ nlp = spacy.load("pt_core_news_sm")
 import pt_core_news_sm
 nlp = pt_core_news_sm.load()
 import os
+import json
+import pickle
 
 #Preprocessamento e indexação do modelo booleano
 #V: Vocabulário, IM: Incidence Matrix, II: Inverted Index e DF: Documents Frequency
@@ -15,41 +17,42 @@ def bm_indexing(text ,n_docs , doc_id, V, IM, II, DF):
     for token in doc:
         if not token.is_stop:
             if not token.is_punct:
-                lemma = token.lemma_.lower()
-                lemma = unicodedata.normalize('NFKD', lemma).encode('ascii', 'ignore').decode('ascii')
+                if not token.is_space:
+                    lemma = token.lemma_.lower()
+                    lemma = unicodedata.normalize('NFKD', lemma).encode('ascii', 'ignore').decode('ascii')
 
-                #Verifica se esta no vocabulario
-                if lemma not in V:                 
+                    #Verifica se esta no vocabulario
+                    if lemma not in V:                 
 
-                    #Adiciona no vocabulario
-                    V[lemma] = len(V)
-                    
-                    #Adiciona uma linha na matriz de incidencia referente ao vocabulario
-                    IM.append( [0] * n_docs ) 
+                        #Adiciona no vocabulario
+                        V[lemma] = len(V)
+                        
+                        #Adiciona uma linha na matriz de incidencia referente ao vocabulario
+                        IM.append( [0] * n_docs ) 
 
-                    #Adiciona o documento no indice invertido
-                    II[lemma] = {doc_id}
-
-                    #Informa a observacao neste documento
-                    IM[ V[lemma] ][doc_id] = 1                                                
-
-                    #Inicia a contagem na frequencia de documento
-                    DF[lemma] = 1
-
-                #Caso ja estaja no vocabulario
-                else:
-                    #Verifica se não foi observado no documento atual
-                    if IM[ V[lemma] ][doc_id] == 0:
-                        #Acrescenta em 1 a frequencia de documento
-                        DF[lemma] += 1
-
-                        #Adiciona o documento
-                        II[lemma].add(doc_id)
+                        #Adiciona o documento no indice invertido
+                        II[lemma] = [doc_id]
 
                         #Informa a observacao neste documento
-                        IM[ V[lemma] ][doc_id] = 1
+                        IM[ V[lemma] ][doc_id] = 1                                                
 
+                        #Inicia a contagem na frequencia de documento
+                        DF[lemma] = 1
 
+                    #Caso ja estaja no vocabulario
+                    else:
+                        #Verifica se não foi observado no documento atual
+                        if IM[ V[lemma] ][doc_id] == 0:
+                            #Acrescenta em 1 a frequencia de documento
+                            DF[lemma] += 1
+
+                            #Adiciona o documento
+                            II[lemma].append(doc_id)
+
+                            #Informa a observacao neste documento
+                            IM[ V[lemma] ][doc_id] = 1
+
+#Diretórios para os documentos
 def get_paths():
     paths = []
 
@@ -65,36 +68,83 @@ def get_paths():
 
     return paths
 
-def get_corpus(paths):
-    corpus = []
+#Leitura de todos os documentos
+def get_document(path):    
 
-    for path in paths:
-        file = open(path,'r')
-        
-        corpus.append(file.read())
-
-        file.close()
+    file = open(path,'r')
     
-    return corpus
+    document = file.read()
 
-def boolean_model(corpus):
-    n_docs = len(corpus)
+    file.close()
+    
+    return document.replace('\n',' ')
+
+def save_data(data,name,obj=False):    
+
+    if obj:
+        file = open(name, 'w')
+        json.dump(data,file)
+
+    else:
+        file = open(name, 'wb')
+        pickle.dump(data,file)
+
+    file.close()
+
+#Modelo booleano de recuperação de informação
+def boolean_model(paths):
+    n_docs = len(paths)
     V = {}
     IM = []
     II = {}
-    DF = {}    
+    DF = {}
 
-    for index, document in enumerate(corpus):
-        bm_indexing(document, n_docs, index, V, IM, II, DF)
+    try:
+        with open('vocabulary', 'r') as f:
+            V = json.load(f)
+        
+        with open('incidentMatrix', 'rb') as f:
+            IM = pickle.load(f)
 
-    for item in II:
-        print(item, II[item])
+        with open('invertedIndex') as f:
+            II = json.load(f)
+        
+        with open('docsFrequency') as f:
+            DF = json.load(f)
+
+    except:
+        for index, path in enumerate(paths):
+            document =  get_document(path)
+
+            if len(document) > 1000000:
+                i = 1000000
+
+                while True:
+                    if document[i] == ' ':
+                        break
+
+                    i -= 1
+
+                
+                pt1 = document[0:i]
+                pt2 = document[i+1:]
+
+                bm_indexing(pt1, n_docs, index, V, IM, II, DF)
+                bm_indexing(pt2, n_docs, index, V, IM, II, DF)
+
+            else:
+                bm_indexing(document, n_docs, index, V, IM, II, DF)        
+
+
+        save_data(V,'vocabulary',obj=True)
+        save_data(IM,'incidentMatrix')
+        save_data(II,'invertedIndex',obj=True)
+        save_data(DF,'docsFrequency',obj=True)
 
 def main():
     paths = get_paths()
-    corpus = get_corpus(paths)
 
-    boolean_model(corpus)
+    boolean_model(paths)
 
 if __name__ == "__main__":
     main()
