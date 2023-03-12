e2fc71173a79e2fa674b8619cc1f2c2f14d3dc5e
Eduardo <eduardo00100@hotmail.com>
2023-03-12 13:17:24 -0300

    Modelo Booleano com similaridade de DICE e metodo de comparacao da similaridade.
    Finalização do modelo booleano com o score de DICE e inserção do método para comparação dos scores no arquivo 'similaridade'.

--- BooleanModel.py
+++ BooleanModel.py
@@ -1,11 +1,16 @@
-import unicodedata
 import spacy
 nlp = spacy.load("pt_core_news_sm")
+
 import pt_core_news_sm
 nlp = pt_core_news_sm.load()
+
 import os
 import json
 import pickle
+
+import Preprocess
+import similaridade
+import numpy as np
 
 #Preprocessamento e indexação do modelo booleano
 #V: Vocabulário, IM: Incidence Matrix, II: Inverted Index e DF: Documents Frequency
@@ -15,42 +20,41 @@ def bm_indexing(text ,n_docs , doc_id, V, IM, II, DF):
 
     #Preprocessamento
     for token in doc:
-        if not token.is_stop:
-            if not token.is_punct:
-                if not token.is_space:
-                    lemma = token.lemma_.lower()
-                    lemma = unicodedata.normalize('NFKD', lemma).encode('ascii', 'ignore').decode('ascii')
+        #Preprocess token
+        p_token = Preprocess.preprocess(token)
 
-                    #Verifica se esta no vocabulario
-                    if lemma not in V:                 
+        if p_token != -1:
 
-                        #Adiciona no vocabulario
-                        V[lemma] = len(V)
-                        
-                        #Adiciona uma linha na matriz de incidencia referente ao vocabulario
-                        IM.append( [0] * n_docs ) 
+            #Verifica se esta no vocabulario
+            if p_token not in V:               
+                
+                #Adiciona no vocabulario
+                V[p_token] = len(V)
+                
+                #Adiciona uma linha na matriz de incidencia referente ao vocabulario
+                IM.append( [0] * n_docs ) 
 
-                        #Adiciona o documento no indice invertido
-                        II[lemma] = [doc_id]
+                #Adiciona o documento no indice invertido
+                II[p_token] = [doc_id]
 
-                        #Informa a observacao neste documento
-                        IM[ V[lemma] ][doc_id] = 1                                                
+                #Informa a observacao neste documento
+                IM[ V[p_token] ][doc_id] = 1                                                
 
-                        #Inicia a contagem na frequencia de documento
-                        DF[lemma] = 1
+                #Inicia a contagem na frequencia de documento
+                DF[p_token] = 1
 
-                    #Caso ja estaja no vocabulario
-                    else:
-                        #Verifica se não foi observado no documento atual
-                        if IM[ V[lemma] ][doc_id] == 0:
-                            #Acrescenta em 1 a frequencia de documento
-                            DF[lemma] += 1
+            #Caso ja estaja no vocabulario
+            else:
+                #Verifica se não foi observado no documento atual
+                if IM[ V[p_token] ][doc_id] == 0:
+                    #Acrescenta em 1 a frequencia de documento
+                    DF[p_token] += 1
 
-                            #Adiciona o documento
-                            II[lemma].append(doc_id)
+                    #Adiciona o documento
+                    II[p_token].append(doc_id)
 
-                            #Informa a observacao neste documento
-                            IM[ V[lemma] ][doc_id] = 1
+                    #Informa a observacao neste documento
+                    IM[ V[p_token] ][doc_id] = 1
 
 #Diretórios para os documentos
 def get_paths():
@@ -79,6 +83,7 @@ def get_document(path):
     
     return document.replace('\n',' ')
 
+#Geração de arquivo com pickle ou json
 def save_data(data,name,obj=False):    
 
     if obj:
@@ -90,6 +95,31 @@ def save_data(data,name,obj=False):
         pickle.dump(data,file)
 
     file.close()
+
+#Obter documentos de uma query
+def get_query(II, V):
+    query = input('O que deseja buscar? ')
+
+    doc =  nlp(query)
+
+    word_list = []
+    docs = {}
+
+    for token in doc:
+        p_token = Preprocess.preprocess( token )
+
+        if p_token != -1:
+            word_list.append( V[p_token] )
+
+            if docs == {}:
+                docs = set( II[ p_token ] )
+
+            else:
+                docs = docs.intersection( set( II[p_token] ) )        
+
+    #print(word_list)
+
+    return docs, word_list
 
 #Modelo booleano de recuperação de informação
 def boolean_model(paths):
@@ -140,6 +170,22 @@ def boolean_model(paths):
         save_data(IM,'incidentMatrix')
         save_data(II,'invertedIndex',obj=True)
         save_data(DF,'docsFrequency',obj=True)
+
+    docs, word_list = get_query(II, V)
+
+    ranking = []
+
+    for doc in docs:
+        indices = np.nonzero( np.array(IM)[:,doc] )[0]
+
+        ranking.append( (paths[doc], similaridade.dice(set(indices), set(word_list))))
+
+    ranking.sort(key=similaridade.my_comp, reverse=True)
+
+    print('Pontuação utilizando DICE score!')
+    for item in ranking:
+        print(f'{item[0]}\t ---\t {item[1]}')       
+
 
 def main():
     paths = get_paths()
--- similaridade.py
+++ similaridade.py
@@ -1,2 +1,5 @@
+def my_comp(tupla):
+    return tupla[1]
+
 def dice(setA, setB):
     return ( 2 * len(setA.intersection(setB)) / ( len(setA) + len(setB) ) )
