d1ddb3ebcfc2269327462f32266d4bc4134e46e8
Eduardo <eduardo00100@hotmail.com>
2023-03-12 21:08:49 -0300

    Similaridade do cosseno
    Método para o cálculo da similaridade do cosseno entre dois vetores.

--- similaridade.py
+++ similaridade.py
@@ -1,5 +1,18 @@
+import numpy as np
+
 def my_comp(tupla):
     return tupla[1]
 
 def dice(setA, setB):
     return ( 2 * len(setA.intersection(setB)) / ( len(setA) + len(setB) ) )
+
+def cosseno(vector1, vector2):
+    dot = np.dot(vector1, vector2)
+
+    sum1 = np.sqrt( np.dot(vector1,vector1) )
+    e_length1 = np.sqrt(sum1)
+
+    sum2 = np.sqrt( np.dot(vector2,vector2) )
+    e_length2 = np.sqrt(sum2)
+
+    return dot/(sum1*sum2)
